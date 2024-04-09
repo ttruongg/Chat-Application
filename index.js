96631ec8ed8eb18d2adde2c74e5ba50c2aa8edb7
ThanhhTruongg1901 <thanhtruong1901@gmail.com>
2024-04-09 22:44:44 +0700

    serving HTML

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-import express from 'express';
-import { createServer } from 'node:http';
-
+import express from "express";
+import { createServer } from "node:http";
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
 const app = express();
 const server = createServer(app);
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello word </h1>');
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.get("/", (req, res) => {
+  res.sendFile(join(__dirname, "index.html"));
 });
 
-
 server.listen(3000, () => {
-    console.log('server is running');
+  console.log("server is running");
 });
