f3c4f04df1006bbc5774f479a802a4142e6e834e
ThanhhTruongg1901 <thanhtruong1901@gmail.com>
2024-04-10 17:09:50 +0700

    emitting events

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ app.get("/", (req, res) => {
 });
 
 io.on("connection", (socket) => {
-  console.log("connected");
-  socket.on("disconnect", () => {
-    console.log("disconnected");
+ // console.log("connected");
+  socket.on("chat message", (msg) => {
+    console.log("message: " + msg);
   });
 });
 
